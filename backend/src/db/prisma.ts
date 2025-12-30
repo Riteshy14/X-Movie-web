ed83e4bfc899a22af38d43d95bfe789d14fd635f
Ritesh Yadav <ritesh.s.yadav@slrtce.in>
2025-12-31 03:55:49 +0530

    Fix Prisma client generation

--- backend/src/db/prisma.ts
+++ backend/src/db/prisma.ts
@@ -1,13 +1,13 @@
 import {PrismaPg} from '@prisma/adapter-pg'
 import dotenv from 'dotenv'
-import { PrismaClient } from '@prisma/client';
+import {PrismaClient } from '@prisma/client'
 
 dotenv.config();
 
 const connectionString = `${process.env.DATABASE_URL}`
 
 const adapter = new PrismaPg({connectionString})
-const Prisma = new PrismaClient({adapter});
+const Prisma = new PrismaClient({adapter})
 
 export {
     Prisma
